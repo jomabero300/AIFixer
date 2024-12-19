dbbafd093587305b9dd18a50b3f5e4edcb8e64c2
Manuel Bello <jomabero300@yahoo.com>
2024-12-19 10:10:04 -0500

    Update CountriesIndex And GenericList withLiterals

--- AIFixer/AIFixer.Frontend/Pages/Countries/CountriesIndex.razor.cs
+++ AIFixer/AIFixer.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -1,11 +1,14 @@
 using AIFixer.Frontend.Repositories;
 using AIFixer.Shared.Entites;
+using AIFixer.Shared.Resources;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 
 namespace AIFixer.Frontend.Pages.Countries;
 
 public partial class CountriesIndex
 {
+    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
     [Inject] private IRepository Repository { get; set; } = null!;
 
     private List<Country>? Countries { get; set; }
--- AIFixer/AIFixer.Frontend/Shared/GenericList.razor.cs
+++ AIFixer/AIFixer.Frontend/Shared/GenericList.razor.cs
@@ -1,9 +1,12 @@
+using AIFixer.Shared.Resources;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 
 namespace AIFixer.Frontend.Shared;
 
 public partial class GenericList<Titem>
 {
+    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
     [Parameter] public RenderFragment? Loading { get; set; }
     [Parameter] public RenderFragment? NoRecords { get; set; }
     [EditorRequired, Parameter] public RenderFragment Body { get; set; } = null!;
